331e527dfd8839282b8a7f5c1375b131651f54e4
Andrew Sunada <kaleb2azriel@gmail.com>
2020-09-20 01:12:24 -0700

    basic adustments to initial react. adding scss

---
+++ src/components/app/app.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './app.module.scss';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
